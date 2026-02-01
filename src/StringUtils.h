64f11acc49b54823e33400d5629cd64439182a51
Giuliano <giuliano@rsic-edipo-805.homenet.telecomitalia.it>
2026-02-01 18:58:13 +0100

    getValue: added unsigned short

--- src/StringUtils.h
+++ src/StringUtils.h
@@ -130,6 +130,7 @@ public:
 			|| std::is_same_v<T, long long>
 			|| std::is_same_v<T, unsigned long long>
 			|| std::is_same_v<T, unsigned int>
+			|| std::is_same_v<T, unsigned short>
 			|| std::is_same_v<T, unsigned long>
 			|| std::is_same_v<T, double>
 			|| std::is_same_v<T, float>
